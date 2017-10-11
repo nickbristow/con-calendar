9faeedc0ab9909ac16bf0bcedab72394aa59bff5
Nick Bristow <Nick.Bristow@gsa.gov>
2017-10-11 12:35:44 -0400

    Allow editing of profile Nav partial Header partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-    @user.save
-    redirect_to users_path
-  end
-
-  def show
-    render 'new'
   end
 
   private
