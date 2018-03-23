a9e3696fa0204db3d932ca380d9741caf71b281e
nickbristow <richard.nick.bristow@gmail.com>
2018-03-23 10:06:01 -0400

    Adding bootstrap

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -43,6 +43,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:name, :description, :date, :start_time, :end_time)
+    params.require(:event).permit(:name, :description, :date, :start_time, :end_time, :category)
   end
 end
