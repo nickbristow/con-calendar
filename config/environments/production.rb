81601a615a791112f20e7ffef69cc9e3c9a62850
nickbristow <richard.nick.bristow@gmail.com>
2018-03-27 16:23:10 -0400

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://geekly-con-calendar.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    domain: "sendgrid.net",
+    port: 587,
+    user_name: "apikey",
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
