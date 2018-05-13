260d61f0d5baf0faf4d719324baef1ca754f3dd0
nickbristow <richard.nick.bristow@gmail.com>
2018-05-13 13:29:38 -0400

    Added raygun

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem 'cancancan', '~> 2.0'
 
 gem 'redcarpet'
 gem 'newrelic_rpm'
+gem 'raygun4ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/raygun.rb
@@ -0,0 +1,7 @@
+Raygun.setup do |config|
+  config.api_key = "tg6PV57H8w6esrgo0jUuCQ=="
+  config.filter_parameters = Rails.application.config.filter_parameters
+
+  # The default is Rails.env.production?
+  # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+end
