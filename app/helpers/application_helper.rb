e24ad21762e53866bca84aff67c0dd6a170749b9
nickbristow <richard.nick.bristow@gmail.com>
2018-05-30 17:42:21 -0400

    Fix bug where bold text on partial event description broke app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,19 @@ module ApplicationHelper
       }
     ]
   end
+
+  def partial_html(html, length, append_text)
+    return html if html.length <= length
+    partial = html[0..length]
+    malformed_tag = partial.rindex(/<\w(?!\w+>)/)
+    partial = partial[0..malformed_tag-1] if malformed_tag
+    opening_tags = partial.scan(/<\w+>/)
+    closing_tags = partial.scan(/<\/\w+>/)
+    opening_tags.map!{|t| t.insert(1, '/')}
+    missing_tags = opening_tags - closing_tags
+    missing_tags.reverse.each do |tag|
+      partial = partial + tag
+    end
+    partial.insert(partial.rindex(/<\/p>/), append_text)
+  end
 end
