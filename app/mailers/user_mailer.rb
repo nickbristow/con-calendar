e378ab36c58854c86f08b650cb0047b49efeeefd
nickbristow <richard.nick.bristow@gmail.com>
2018-07-15 12:58:03 -0400

    removed crap

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,4 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(user, template)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
-
   def notice_email(user, subject, body, events, day)
     @user = user
     @body = body
