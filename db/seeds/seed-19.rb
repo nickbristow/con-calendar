37cd60359592ac03384df1f13472040933417949
nickbristow <richard.nick.bristow@gmail.com>
2019-05-22 19:43:18 -0400

    Update ruby version. Add custom seed files

---
+++ db/seeds/seed-19.rb
@@ -0,0 +1,10 @@
+require 'csv'
+
+# Setup Events
+csv_text = File.read('gc19-events.csv')
+csv = CSV.parse(csv_text, headers: true)
+csv.each do |row|
+  row_hash = row.to_hash
+  puts row_hash
+  Event.create!(row_hash)
+end
