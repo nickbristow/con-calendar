c824f5b0944ff7ad3f4f0aebc459adff8ac1ffad
nickbristow <richard.nick.bristow@gmail.com>
2019-05-23 11:31:37 -0400

    Add image support

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -116,6 +116,6 @@ class EventsController < ApplicationController
     if params[:event][:owner_id].blank? || !current_user.admin?
       params[:event][:owner_id] = current_user.id
     end
-    params.require(:event).permit(:name, :description, :date, :start_time, :end_time, :category, :max_attendees, :owner_id, :location)
+    params.require(:event).permit(:name, :description, :date, :start_time, :end_time, :category, :max_attendees, :owner_id, :location, :image)
   end
 end
