3c6dcf706b14e74151c984af455cea33ed0d9939
nickbristow <richard.nick.bristow@gmail.com>
2019-07-16 19:25:47 -0400

    Added more conflicting event categories

--- app/models/event.rb
+++ app/models/event.rb
@@ -171,12 +171,34 @@ class Event < ApplicationRecord
     URI.encode(str).gsub('&', '%26')
   end
 
+  # def conflicting_events
+  #   Event.where(start_time: start_time...end_time)
+  #        .or(Event.where(end_time: start_time...end_time))
+  #        .or(Event.where('start_time < ? AND end_time > ?', start_time, end_time))
+  #        .where.not(id: id)
+  #        .where(date: date)
+  #        .order(:start_time).category_order.order(:id)
+  # end
+
   def conflicting_events
-    Event.where(start_time: start_time...end_time)
-         .or(Event.where(end_time: start_time...end_time))
-         .or(Event.where('start_time < ? AND end_time > ?', start_time, end_time))
-         .where.not(id: id)
-         .where(date: date)
-         .order(:start_time).category_order.order(:id)
+    Event.where('start_time > ? AND start_time < ?', start_time, end_time)
+      .or(Event.where('end_time > ? AND end_time < ?', start_time, end_time))
+      .where.not(id: id)
+      .where(date: date)
+      .order(:start_time).category_order.order(:id)
+  end
+
+  def prior_events
+    Event.where(end_time: start_time)
+      .where.not(id: id)
+      .where(date: date)
+      .order(:start_time).category_order.order(:id)
+  end
+
+  def upcoming_events
+    Event.where(start_time: end_time)
+      .where.not(id: id)
+      .where(date: date)
+      .order(:start_time).category_order.order(:id)
   end
 end
