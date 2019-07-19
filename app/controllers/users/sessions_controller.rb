e99e40f2a2df6f9e966509f7e2bbf4ed6db1dc5d
nickbristow <richard.nick.bristow@gmail.com>
2019-07-19 18:14:02 -0400

    Bug fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
+  skip_before_filter :verify_authenticity_token, :only => :create
 
   # GET /resource/sign_in
   def new
